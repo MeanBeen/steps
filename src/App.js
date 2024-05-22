3fcb2e76b7504ac6910f21cbbae232befc822478
Khushal Khan <khushalkhan40@gmail.com>
2024-05-22 12:37:28 +0500

    added usestate

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button, Layout, Typography } from "antd";
 
 const { Content } = Layout;
@@ -10,22 +11,42 @@ const messages = [
 ];
 
 const App = () => {
-  const step = 1;
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handlePrevious = () => {
+    if (step > 1) setStep((s) => s - 1);
+  };
+  const handleNext = () => {
+    if (step < 3) setStep((s) => s + 1);
+  };
+
   return (
-    <Content className="steps">
-      <Content className="numbers">
-        <Content className={`${step >= 1 ? "active" : ""}`}>1</Content>
-        <Content className={`${step >= 2 ? "active" : ""}`}>2</Content>
-        <Content className={`${step >= 3 ? "active" : ""}`}>3</Content>
-      </Content>
-      <Text className="message">
-        Step {step}: {messages[step - 1]}
-      </Text>
-      <Content className="buttons">
-        <Button className="buttons">Previous</Button>
-        <Button className="buttons">Next</Button>
-      </Content>
-    </Content>
+    <>
+      <Button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </Button>
+      {isOpen && (
+        <Content className="steps">
+          <Content className="numbers">
+            <Content className={`${step >= 1 ? "active" : ""}`}>1</Content>
+            <Content className={`${step >= 2 ? "active" : ""}`}>2</Content>
+            <Content className={`${step >= 3 ? "active" : ""}`}>3</Content>
+          </Content>
+          <Text className="message">
+            Step {step}: {messages[step - 1]}
+          </Text>
+          <Content className="buttons">
+            <Button className="buttons" onClick={handlePrevious}>
+              Previous
+            </Button>
+            <Button className="buttons" onClick={handleNext}>
+              Next
+            </Button>
+          </Content>
+        </Content>
+      )}
+    </>
   );
 };
 
